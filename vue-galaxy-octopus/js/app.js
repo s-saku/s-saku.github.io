a7d04a6f727e67d92f6d9b5b9917959240d28a63
Shinya Sakurai <t.nishi7@gmail.com>
2016-11-27 21:38:58 +0900

    vue-galaxy-octopus

---
+++ vue-galaxy-octopus/js/app.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import GallaxyOctopus from './components/GallaxyOctopus.vue'
+
+var app = new Vue({
+
+  el: '#app',
+
+  components: {
+    GallaxyOctopus
+  },
+
+  data: {
+    showMobileMenu: false,
+    showGalleryModal: false
+  },
+
+  created () {
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+
+  }
+})
+
+export default app
